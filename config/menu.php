64fc485ea97259fe9f705ac35819f2bf75c895b2
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-23 15:41:31 +0200

    add menu.php

---
+++ config/menu.php
@@ -0,0 +1,44 @@
+<?php
+
+return linkDC: [
+    {
+      name: "CHARACTERS",
+      src: "#",
+    },
+    {
+      name: "COMICS",
+      src: "#",
+    },
+    {
+      name: "MOVIES",
+      src: "#",
+    },
+    {
+      name: "TV",
+      src: "#",
+    },
+    {
+      name: "GAMES",
+      src: "#",
+    },
+    {
+      name: "COLLECTIBLES",
+      src: "#",
+    },
+    {
+      name: "VIDEOS",
+      src: "#",
+    },
+    {
+      name: "FANS",
+      src: "#",
+    },
+    {
+      name: "NEWS",
+      src: "#",
+    },
+    {
+      name: "SHOP",
+      src: "#",
+    },
+],
