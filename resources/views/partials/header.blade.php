c32a43ae026ed9b8c3c1d3c228b225b12f6c416a
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-23 16:34:44 +0200

    add li with foreach

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,13 @@
 <header>
     <div class="content-header">
-        {{-- <img src="{{ Vite::asset('../resources/img/dc-logo.png') }}" alt=""> --}}
+        <img src="{{ Vite::asset('../resources/img/dc-logo.png') }}" alt="">
         <ul class="list-header-link">
             @php
                 $menu = config('menu');
             @endphp
+            @foreach ($menu as $item)
+                <li><a href="">{{ $item['name'] }}</a></li>
+            @endforeach
         </ul>
     </div>
 </header>
