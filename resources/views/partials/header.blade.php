297bd661a2b03cc51365eeb1c21d3f23d50ce6e3
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-23 15:21:35 +0200

    add header.blade.php

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="content-header">
+        <img src="{{ Vite::asset('../resources/img/dc-logo.png') }}" alt="">
+        <ul class="list-header-link">
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </div>
+</header>
