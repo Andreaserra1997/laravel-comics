43fde438faa1c2e39f13cec150d9fbc6f1171e0a
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-23 16:07:19 +0200

    add menu in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,10 @@
 <header>
     <div class="content-header">
-        <img src="{{ Vite::asset('../resources/img/dc-logo.png') }}" alt="">
+        {{-- <img src="{{ Vite::asset('../resources/img/dc-logo.png') }}" alt=""> --}}
         <ul class="list-header-link">
-            <li>CHARACTERS</li>
-            <li>COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAMES</li>
-            <li>COLLECTIBLES</li>
-            <li>VIDEOS</li>
-            <li>FANS</li>
-            <li>NEWS</li>
-            <li>SHOP</li>
+            @php
+                $menu = config('menu');
+            @endphp
         </ul>
     </div>
 </header>
