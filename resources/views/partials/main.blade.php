8f819afe4dc84fce741b15b83fda0123ca75cc4f
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-23 17:10:59 +0200

    add comics.php

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,11 +1,13 @@
 <div class="container-card">
     <ul class="list-card">
         @php
-            $card = config('card');
+            $comics = config('comics');
         @endphp
-        @foreach ($card as $item)
-        <img src="{{ $item['thumb'] }}" alt="">
-            <li><a href="">{{ $item['series'] }}</a></li>
+        @foreach ($comics as $item)
+            <li>
+                <img src="{{ $item['thumb'] }}" alt="">
+                <a href="">{{ $item['series'] }}</a>
+            </li>
         @endforeach
     </ul>
 </div>
