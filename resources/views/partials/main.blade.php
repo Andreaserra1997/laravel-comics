816ab75b3e0855f5fa25a48c68f8415e38f071da
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-24 12:13:33 +0200

    add img buy dc

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -5,9 +5,36 @@
         @endphp
         @foreach ($comics as $item)
             <li>
-                <img src="{{ Vite::asset('resources/img/'.$item['thumb']) }}" alt="">
+                <img src="{{ $item['thumb'] }}" alt="">
                 <a href="">{{ $item['series'] }}</a>
             </li>
         @endforeach
+    </ul>
+    <div class="btn">
+        <a>LOAD MORE</a>
+    </div>
+</div>
+<div class="content-down">
+    <ul>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+            <a href="#">DIGITAL COMICS</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+            <a href="#">DC MERCHANDISE</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+            <a href="#">SUBSCRIPTION</a>
+        </li>
+        <li>
+            <img class="location" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+            <a href="#">COMIC SHOP LOCATOR</a>
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+            <a href="#">DC POWER VISA</a>
+        </li>
     </ul>
 </div>
